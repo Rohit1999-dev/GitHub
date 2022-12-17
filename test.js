7d00206c3c2a7f7362ae44f7c09e02d8a1f0f7ea
abhinav7332 <rohit18@navgurukul.org>
2022-12-17 23:47:23 +0530

    Added few line of code inside the file.

--- test.js
+++ test.js
@@ -1,1 +1,9 @@
 console.log("about git !");
+// github is one of the best platform where you deployed your project & keep shared safe.
+var github = 'yes I want learn';
+
+if(github == 'yes I want learn'){
+    console.log('hurrah !');
+}else{
+    console.log('oh set !');
+}
